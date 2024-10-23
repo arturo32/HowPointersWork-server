de9837257a4d583ff6022f4050a1c75132d36524
Arturo Fonseca <arturofonsecas32@gmail.com>
2024-10-23 07:49:34 -0300

    feat: add initial files

---
+++ go.mod
@@ -0,0 +1,8 @@
+module example.com/server
+
+go 1.23.2
+
+require (
+	github.com/pkg/errors v0.9.1
+	github.com/runabol/tork v0.1.99
+)
---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"github.com/pkg/errors"
+	"github.com/runabol/tork/cli"
+	"github.com/runabol/tork/engine"
+	"github.com/runabol/tork/input"
+	"github.com/runabol/tork/middleware/web"
+	"net/http"
+	"os"
+
+	"github.com/runabol/tork/conf"
+)
+
+func main() {
+	// Load the Tork config file (if exists)
+	if err := conf.LoadConfig(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	engine.RegisterEndpoint(http.MethodPost, "/myendpoint", handler)
+
+	if err := cli.New().Run(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+type ExecRequest struct {
+	Code     string `json:"code"`
+	Language string `json:"language"`
+}
+
+func handler(c web.Context) error {
+	req := ExecRequest{}
+	if err := c.Bind(&req); err != nil {
+		c.Error(http.StatusBadRequest, err)
+
+		return nil
+	}
+
+	task, err := buildTask(req)
+	if err != nil {
+		c.Error(http.StatusBadRequest, err)
+		return nil
+	}
+
+	taskInput := &input.Job{
+		Name:  "code execution",
+		Tasks: []input.Task{task},
+	}
+
+	job, err := engine.SubmitJob(c.Request().Context(), taskInput)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("job %s submitted!\n", job.ID)
+
+	return c.JSON(http.StatusOK, req)
+}
+
+func buildTask(er ExecRequest) (input.Task, error) {
+	var image string
+	var run string
+	var filename string
+
+	switch er.Language {
+	case "":
+		return input.Task{}, errors.Errorf("require: language")
+	case "python":
+		image = "python:3"
+		filename = "script.py"
+		run = "python script.py > $TORK_OUTPUT"
+	case "go":
+		image = "golang:1.19"
+		filename = "main.go"
+		run = "go run main.go > $TORK_OUTPUT"
+	case "bash":
+		image = "alpine:3.18.3"
+		filename = "script"
+		run = "sh ./script > $TORK_OUTPUT"
+	default:
+		return input.Task{}, errors.Errorf("unknown language: %s", er.Language)
+	}
+
+	return input.Task{
+		Name:  "execute code",
+		Image: image,
+		Run:   run,
+		Files: map[string]string{
+			filename: er.Code,
+		},
+	}, nil
+}
