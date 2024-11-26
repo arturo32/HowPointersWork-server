a4140f2e260fb3cee03a3ee7831c94296dfcb807
Arturo Fonseca <arturofonsecas32@gmail.com>
2024-11-26 12:46:47 -0300

    add: c++ support

--- handler/handler.go
+++ handler/handler.go
@@ -81,42 +81,32 @@ func buildTask(er ExecRequest) (input.Task, error) {
 	var image string
 	var run string
 	var filename string
-	var gdbCommands string
 
 	switch strings.TrimSpace(er.Language) {
 	case "":
 		return input.Task{}, errors.Errorf("require: language")
-	case "python":
-		image = "python:3"
-		filename = "script.py"
-		run = "python script.py > $TORK_OUTPUT"
-	case "go":
-		image = "golang:1.19"
-		filename = "main.go"
-		run = "go run main.go > $TORK_OUTPUT"
-	case "bash":
-		image = "alpine:3.18.3"
-		filename = "script"
-		run = "sh ./script > $TORK_OUTPUT"
+	case "c++":
+		image = "gcc-compiler:latest"
+		filename = "usercode.cpp"
+		run = "mv usercode.cpp /tmp/user_code/usercode.cpp; " +
+			"g++ -ggdb -O0 -fno-omit-frame-pointer -o /tmp/user_code/usercode /tmp/user_code/usercode.cpp; "
+
+		if debug_valgrind {
+			run += "cat /tmp/user_code/usercode.vgtrace > $TORK_OUTPUT"
+		} else {
+			run += "python3 /tmp/parser/wsgi_backend.py c++ > $TORK_OUTPUT"
+		}
 	case "c":
-		image = "gcc:latest"
-		filename = "main.c"
-		run = "gcc main.c -o main;./main > $TORK_OUTPUT"
-	case "gdb":
 		image = "gcc-compiler:latest"
 		filename = "usercode.c"
-		gdbCommands = "d.gdb"
-		//
 		run = "mv usercode.c /tmp/user_code/usercode.c; " +
 			"gcc -ggdb -O0 -fno-omit-frame-pointer -o /tmp/user_code/usercode /tmp/user_code/usercode.c; "
 
 		if debug_valgrind {
-			run += "python3 /tmp/parser/wsgi_backend.py c > $TORK_OUTPUT"
+			run += "cat /tmp/user_code/usercode.vgtrace > $TORK_OUTPUT"
 		} else {
 			run += "python3 /tmp/parser/wsgi_backend.py c > $TORK_OUTPUT"
 		}
-
-		//
 
 	default:
 		return input.Task{}, errors.Errorf("unknown language: %s", er.Language)
@@ -132,8 +122,7 @@ func buildTask(er ExecRequest) (input.Task, error) {
 			Memory: "2000m",
 		},
 		Files: map[string]string{
-			filename:    er.Code,
-			gdbCommands: "set disable-randomization off\nset auto-solib-add off\nstart\nn\nbt full\n",
+			filename: er.Code,
 		},
 	}, nil
 }
